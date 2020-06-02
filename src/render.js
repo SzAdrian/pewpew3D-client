8bedb9f863ddc932ce959fd814df156b66833155
SzAdrian <51090249+SzAdrian@users.noreply.github.com>
2020-06-02 16:55:39 +0200

    renderloop on server

--- src/render.js
+++ src/render.js
@@ -1,6 +1,7 @@
 import { socket } from "./io";
 import { players } from "./players";
 import { cursor } from "./controll";
+import { render } from "@testing-library/react";
 
 var context = document.getElementById("game").getContext("2d");
 
@@ -67,3 +68,31 @@ export let renderPlayers = () => {
 export function getAngle(x1, x2, y1, y2) {
   return 180 - Math.atan2(y1 - y2, x1 - x2) * (180 / Math.PI) * -1;
 }
+function renderr(players) {
+  context.clearRect(
+    0,
+    0,
+    document.getElementById("game").width,
+    document.getElementById("game").height
+  );
+  Object.keys(players).map((id) => {
+    let player = players[id];
+
+    //draw gun
+    context.beginPath();
+    context.moveTo(player.x, player.y);
+    context.lineTo(
+      player.x + 20 * Math.cos((Math.PI * player.angle) / 180),
+      player.y + 20 * Math.sin((Math.PI * player.angle) / 180)
+    );
+    context.stroke();
+    //draw player
+    context.beginPath();
+    context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
+    context.fillStyle = id === socket.id ? "green" : "black";
+    context.fill();
+  });
+}
+socket.on("render", (data) => {
+  renderr(data);
+});
