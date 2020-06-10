f978b4f42f90eff567e0a282555c56a8fd84493a
SzAdrian <51090249+SzAdrian@users.noreply.github.com>
2020-06-10 17:23:53 +0200

    refactored things

--- src/render.js
+++ src/render.js
@@ -3,14 +3,24 @@ import { cursor } from "./controll";
 
 var context = document.getElementById("game").getContext("2d");
 export let players = {};
-let walls = [
-  { x1: 100, y1: 0, x2: 100, y2: 100 },
-  { x1: 100, y1: 100, x2: 300, y2: 100 },
-  { x1: 300, y1: 100, x2: 300, y2: 400 },
-];
+let walls = [];
 const X = 320;
 const Y = 240;
+let offsetX = 0;
+let offsetY = 0;
+function drawBullets(data) {
+  data.bullets.map((bullet) => {
+    context.beginPath();
+    context.fillStyle = bullet.socket === socket.id ? "green" : "black";
+    context.arc(bullet.x, bullet.y, 3, 0, 2 * Math.PI);
+    context.fill();
+  });
+}
 
+function setAngle(player) {
+  player.angle = getAngle(player.x, cursor.x, player.y, cursor.y);
+  socket.emit("angle", player.angle);
+}
 function drawWalls() {
   walls.map((wall) => {
     context.beginPath();
@@ -38,31 +48,6 @@ function drawPlayer(player, id) {
     player.y + 20
   );
 }
-function drawPlayerNew(player, id, offsetX, offsetY) {
-  if (id === socket.id) {
-    player.x = X;
-    player.y = Y;
-    offsetX = 0;
-    offsetY = 0;
-  }
-  context.beginPath();
-  context.arc(player.x - offsetX, player.y - offsetY, 10, 0, 2 * Math.PI);
-  context.fillStyle = id === socket.id ? "green" : "black";
-  context.fill();
-  context.font = "12px Arial";
-  context.fillText(
-    player.name,
-    player.x - offsetX - player.name.length * 3,
-    player.y - offsetY - 20
-  );
-
-  context.fillStyle = "red";
-  context.fillText(
-    `HP: ${player.health}`,
-    player.x - offsetX - player.name.length * 3,
-    player.y - offsetY + 20
-  );
-}
 
 function drawGun(player) {
   context.beginPath();
@@ -71,17 +56,6 @@ function drawGun(player) {
   context.lineTo(
     player.x + 20 * Math.cos((Math.PI * player.angle) / 180),
     player.y + 20 * Math.sin((Math.PI * player.angle) / 180)
-  );
-  context.stroke();
-}
-
-function drawGunNew(player, offsetX, offsetY) {
-  context.beginPath();
-  context.lineWidth = 2;
-  context.moveTo(player.x - offsetX, player.y - offsetY);
-  context.lineTo(
-    player.x - offsetX + 20 * Math.cos((Math.PI * player.angle) / 180),
-    player.y - offsetY + 20 * Math.sin((Math.PI * player.angle) / 180)
   );
   context.stroke();
 }
@@ -98,27 +72,98 @@ function clearCanvas() {
 export function getAngle(x1, x2, y1, y2) {
   return 180 - Math.atan2(y1 - y2, x1 - x2) * (180 / Math.PI) * -1;
 }
+//=====================================================================================================================================================
+function drawWallsNew() {
+  walls.map((wall) => {
+    context.beginPath();
+    context.lineWidth = 10;
+    context.moveTo(wall.x1 - offsetX, wall.y1 - offsetY);
+    context.lineTo(wall.x2 - offsetX, wall.y2 - offsetY);
+    context.stroke();
+  });
+}
+
+function drawGunNew(player) {
+  context.beginPath();
+  context.lineWidth = 2;
+  context.moveTo(player.x - offsetX, player.y - offsetY);
+  context.lineTo(
+    player.x - offsetX + 20 * Math.cos((Math.PI * player.angle) / 180),
+    player.y - offsetY + 20 * Math.sin((Math.PI * player.angle) / 180)
+  );
+  context.stroke();
+}
+
+function drawPlayerNew(player, id, offsetX, offsetY) {
+  if (id === socket.id) {
+    player.x = X;
+    player.y = Y;
+    offsetX = 0;
+    offsetY = 0;
+  }
+  context.beginPath();
+  context.arc(
+    player.x - offsetX,
+    player.y - offsetY,
+    player.size,
+    0,
+    2 * Math.PI
+  );
+  drawName(id, player, offsetX, offsetY);
+  drawHP(player, offsetX, offsetY);
+}
+function drawHP(player, offsetX, offsetY) {
+  context.fillStyle = "red";
+  context.fillText(
+    `HP: ${player.health}`,
+    player.x - offsetX - player.name.length * 3,
+    player.y - offsetY + player.size * 2
+  );
+}
+
+function drawName(id, player, offsetX, offsetY) {
+  context.fillStyle = id === socket.id ? "green" : "black";
+  context.fill();
+  context.font = "12px Arial";
+  context.fillText(
+    player.name,
+    player.x - offsetX - player.name.length * 3,
+    player.y - offsetY - player.size * 2
+  );
+}
+
+function setAngleNew(player) {
+  player.angle = getAngle(X, cursor.x, Y, cursor.y);
+  socket.emit("angle", player.angle);
+}
+function drawBulletsNew(data) {
+  data.bullets.map((bullet) => {
+    context.beginPath();
+    context.fillStyle = bullet.socket === socket.id ? "green" : "black";
+    context.arc(
+      bullet.x - offsetX,
+      bullet.y - offsetY,
+      bullet.size,
+      0,
+      2 * Math.PI
+    );
+    context.fill();
+  });
+}
 function render(data) {
-  let offsetX = data.players[socket.id].x - X;
-  let offsetY = data.players[socket.id].y - Y;
+  offsetX = data.players[socket.id].x - X;
+  offsetY = data.players[socket.id].y - Y;
 
   clearCanvas();
-  drawBulletsNew(data, offsetX, offsetY);
-  //drawBullets(data);
-  //drawWalls();
-
+  drawBulletsNew(data);
+  drawWallsNew();
   Object.keys(data.players).map((id) => {
     let player = data.players[id];
-
     const isLocalPlayer = id === socket.id;
     if (isLocalPlayer) {
       setAngleNew(player);
-      //setAngle(player);
     }
-
-    //drawGun(player);
-    //drawPlayer(player, id);
-    drawGunNew(player, offsetX, offsetY);
+    drawGunNew(player);
     drawPlayerNew(player, id, offsetX, offsetY);
   });
 }
@@ -127,29 +172,7 @@ socket.on("render", (data) => {
   players = data.players;
   render(data);
 });
-function drawBullets(data) {
-  data.bullets.map((bullet) => {
-    context.beginPath();
-    context.fillStyle = bullet.socket === socket.id ? "green" : "black";
-    context.arc(bullet.x, bullet.y, 3, 0, 2 * Math.PI);
-    context.fill();
-  });
-}
-function drawBulletsNew(data, offsetX, offsetY) {
-  data.bullets.map((bullet) => {
-    context.beginPath();
-    context.fillStyle = bullet.socket === socket.id ? "green" : "black";
-    context.arc(bullet.x - offsetX, bullet.y - offsetY, 3, 0, 2 * Math.PI);
-    context.fill();
-  });
-}
 
-function setAngle(player) {
-  player.angle = getAngle(player.x, cursor.x, player.y, cursor.y);
-  socket.emit("angle", player.angle);
-}
-
-function setAngleNew(player) {
-  player.angle = getAngle(X, cursor.x, Y, cursor.y);
-  socket.emit("angle", player.angle);
-}
+socket.on("map", (data) => {
+  walls = data;
+});
