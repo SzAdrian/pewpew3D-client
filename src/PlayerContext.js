31e8c3b0f524d441f26ae075b70ed97c074a4387
SzAdrian <51090249+SzAdrian@users.noreply.github.com>
2020-06-15 19:27:54 +0200

    wall width

---
+++ src/PlayerContext.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState } from "react";
+import { createContext } from "react";
+
+export const PlayerContext = createContext();
+
+export function PlayerProvider(props) {
+  const [player, setPlayer] = useState({});
+
+  return (
+    <PlayerContext.Provider value={player}>
+      {props.children}
+    </PlayerContext.Provider>
+  );
+}
+
+export default PlayerContext;
